b99c148ac1b1b33079babf920a0f60d224352e39
Andrei Radu <andrei.agg95@yahoo.com>
2023-11-02 12:45:21 +0200

    Add home, footer, header components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  onLogout() {
+
+  }
+}
